9be06829ad452b024e9883085d9d8ae6e68f9e9e
Ozzie Neher <oz.neher@gmail.com>
2020-08-30 10:54:59 -0600

    ISO/Human results. UTC/Local results

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,47 @@
 import Head from 'next/head';
-import formatISO from 'date-fns/formatISO';
 import { useState } from 'react';
-import isValid from 'date-fns/isValid';
+import moment, { Moment } from 'moment';
+
+interface ResultProps {
+  label: string;
+  value: string;
+}
+
+function Result({ label, value }: ResultProps) {
+  return (
+    <div className={'mt-4'}>
+      <p className="max-w-2xl text-sm leading-5 text-gray-600">{label}</p>
+      <h3 className="mt-1 text-lg leading-6 font-medium text-gray-900">
+        {value}
+      </h3>
+    </div>
+  );
+}
+
+function setTz(date: Moment, isUtc: boolean) {
+  if (isUtc) {
+    return date.clone().utc();
+  }
+  return date.clone();
+}
 
 export default function Home() {
   const [unixInput, setUnixInput] = useState('');
-  const [result, setResult] = useState('');
+  const [resultDate, setResultDate] = useState<Moment | null>(null);
+  const [utc, setUtc] = useState(false);
 
   function onUnixInputChange(e: React.FormEvent<HTMLInputElement>) {
+    setResultDate(null);
     setUnixInput(e.currentTarget.value);
   }
 
-  function onConvertPress() {
-    const date = new Date(Number(unixInput) * 1000);
-    if (!unixInput || !isValid(date)) {
+  function onConvertPress(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const date = moment.unix(+unixInput);
+    if (!unixInput || !date.isValid()) {
       return;
     }
-    setResult(formatISO(date));
+    setResultDate(date);
   }
 
   return (
@@ -27,25 +52,74 @@ export default function Home() {
       </Head>
 
       <div className={'px-4 w-full max-w-md mx-auto'}>
-        <label htmlFor="" className={'block mb-2 text-lg'}>
-          Enter a timestamp
-        </label>
-        <input
-          type="text"
-          className={
-            'block w-full px-2 py-1 border border-gray-700 border-solid rounded-md'
-          }
-          value={unixInput}
-          onChange={onUnixInputChange}
-        />
-        <button
-          className={'mt-2 block w-full py-2 bg-black text-white rounded-md'}
-          onClick={onConvertPress}
-        >
-          Convert
-        </button>
+        <form onSubmit={onConvertPress}>
+          <label htmlFor="" className={'block mb-2 text-lg'}>
+            Enter a timestamp
+          </label>
+          <input
+            type="text"
+            className={
+              'block w-full px-2 py-1 border border-gray-700 border-solid rounded-md'
+            }
+            value={unixInput}
+            onChange={onUnixInputChange}
+          />
+          <button
+            className={'mt-2 block w-full py-2 bg-black text-white rounded-md'}
+            type={'submit'}
+          >
+            Convert
+          </button>
+        </form>
 
-        <p className={'mt-2 text-gray-700'}>{result}</p>
+        {resultDate && (
+          <div className={'mt-4'}>
+            <hr className={'mb-4'} />
+            <div className="flex flex-row space-x-4">
+              <div className="flex items-center">
+                <input
+                  id="tz-utc"
+                  type="radio"
+                  name="local"
+                  className="form-radio h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"
+                  checked={utc === true}
+                  onChange={() => setUtc(true)}
+                  tabIndex={0}
+                />
+                <label htmlFor="tz-utc" className="ml-3">
+                  <span className="block text-sm leading-5 font-medium text-gray-700">
+                    UTC
+                  </span>
+                </label>
+              </div>
+              <div className="flex items-center">
+                <input
+                  id="tz-local"
+                  name="local"
+                  type="radio"
+                  className="form-radio h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"
+                  checked={utc === false}
+                  onChange={() => setUtc(false)}
+                  tabIndex={0}
+                />
+                <label htmlFor="tz-local" className="ml-3">
+                  <span className="block text-sm leading-5 font-medium text-gray-700">
+                    Local Timezone (UTC-{new Date().getTimezoneOffset() / 60})
+                  </span>
+                </label>
+              </div>
+            </div>
+            <div className="space-y-4">
+              <Result label={'ISO-8601'} value={resultDate.toISOString(!utc)} />
+              <Result
+                label={'Human Readable'}
+                value={setTz(resultDate, utc).format(
+                  'MMMM Do, YYYY - hh:mm:ss a',
+                )}
+              />
+            </div>
+          </div>
+        )}
       </div>
 
       <style jsx>{``}</style>
